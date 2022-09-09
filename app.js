8b68b4a3468a5a617b4d5035c7906e76e184d2e3
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-09 09:56:03 +0100

    inital route working with app.use and router.get

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
+import router from "./routes/routes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/");
+app.use("/", router);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);

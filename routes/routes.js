d12310ce61648f52d3526766c81195b9355e19af
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-09 10:22:13 +0100

    preliminary routes written, each needs a hefty refactor

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,25 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Routes working")
+//test create route
+router.get("/create", (req, res) => {
+    res.send("Create/POST route working")
+})
+
+//test read route, refactor for correct route
+router.get("/read", (req, res) => {
+    res.send("Read/GET route working")
+})
+
+
+//test update route
+router.get("/update", (req, res) => {
+    res.send("Update/PUT/PATCH route working")
+})
+
+//test delete route
+router.get("/delete", (req, res) => {
+    res.send("Delete/DELETE route working")
 })
 
 
